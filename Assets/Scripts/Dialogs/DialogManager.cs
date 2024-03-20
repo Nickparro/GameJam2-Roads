688f788ca802287ed34050a2ba850d927523a4d3
carlos4rias <carlos4rias@users.noreply.github.com>
2024-03-20 12:55:26 -0500

    Dialog Manager basic functionality

--- Assets/Scripts/Dialogs/DialogManager.cs
+++ Assets/Scripts/Dialogs/DialogManager.cs
@@ -1,22 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class DialogManager : MonoBehaviour
 {
+    public static DialogManager instance;
     Dictionary<string, string> dialogsMap = new Dictionary<string, string>();
+    private GameObject dialogPanelUi;
+    [SerializeField]
+    private TextMeshProUGUI dialogText;
+    private bool dialogState = false;
     
+    private void Awake() {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
 
-    void Start()
-    {
+        dialogPanelUi = GameObject.FindGameObjectWithTag("Dialog");
+        dialogPanelUi.SetActive(false);
         dialogsMap.Add("trigger1", "First dialog");
         dialogsMap.Add("trigger2", "Second dialog");
     }
 
-
-    public void HandleDialog(string dialog) {
-        Debug.Log(dialogsMap[dialog]);
+    public void HandleShowDialog(string dialog) {
+        toggleDialogState(true);
+        dialogText.text = dialogsMap[dialog];
     }
 
+
+    public void HandleHideDialog() {
+        toggleDialogState(false);
+    }
+
+    private void toggleDialogState(bool newState) {
+
+        dialogState = newState;
+        dialogPanelUi.SetActive(dialogState);
+    }
 
 }
